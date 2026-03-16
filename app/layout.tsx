341b252c0e7ada5056137dcca588d123b0734e58
rusty159753 <76086073+rusty159753@users.noreply.github.com>
2026-03-16 15:26:03 -0600

    Remove unused RootLayout component and cookies import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,3 @@
+import "./globals.css"
 import { createServerClient } from '@/utils/supabase/server';
-import { cookies } from 'next/headers';
-
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const cookieStore = cookies();
-
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
+import { cookies } from '
