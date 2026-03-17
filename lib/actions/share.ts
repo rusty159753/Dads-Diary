9e184eda65d95eae0962008f3c7ab0b0c9a51e7a
rusty159753 <76086073+rusty159753@users.noreply.github.com>
2026-03-17 12:18:05 -0600

    Update share.ts

--- lib/actions/share.ts
+++ lib/actions/share.ts
@@ -1,7 +1,6 @@
-'use server';
+"use server";
 
 export async function createShareToken(entryId: string) {
-  // TODO: Real DB later
   console.log('Sharing entry:', entryId);
-  return 'demo-' + entryId.slice(-4);
+  return "demo-" + entryId.slice(-4);
 }
